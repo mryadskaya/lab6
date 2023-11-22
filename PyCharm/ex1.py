59665dfba09473acb20204130b6b3e945fa9b1e2
mryadskaya <125180218+mryadskaya@users.noreply.github.com>
2023-11-22 12:22:40 +0300

    Delete PyCharm directory

--- PyCharm/ex1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__=='__main__':
-    b=input("введите предложение:")
-    t=b.count("о")
-    print("количество букв о в предложении:",t)
--- PyCharm/ex4.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__ =='__main__':
-    sentence = input("Введите предложение: ")
-    words = sentence.split()
-    for word in words:
-        if word != "привет":
-            print(word)
